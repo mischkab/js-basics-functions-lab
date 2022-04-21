a573ec58439b2db5f59af9a9a4bf9937b30f4a2a
Mischka Booley <mischka@gearbox.com.au>
2022-04-21 21:15:05 +1000

    complete basic functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup, hq = 42) {
+  return Math.abs(hq - pickup);
+}
+
+function distanceFromHqInFeet(pickup, hq = 42) {
+  return (distanceFromHqInBlocks(pickup, hq) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return (Math.abs(start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let total = distanceTravelledInFeet(start, destination);
+  if (total <= 400) {
+    return 0
+  } else if (total < 2000) {
+    total -= 400;
+    return total * 0.02;
+  } else if (total < 2500) {
+    total -= 400;
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
